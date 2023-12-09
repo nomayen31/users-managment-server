c47d67685b8aac3f37725d3bc35beae3e1bc487d
nomayen <nomayen.ohin@gmail.com>
2023-12-09 12:31:38 +0600

    sarver to client side data pass and store

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const app = express();
 const  cors = require('cors')
+
+// middleware 
+app.use(express.json())
 app.use(cors())
 
 const port = process.env.PORT || 5000;
 const users = [
     {id:1, name:'shahanika', email:'shahanika@gmail.com'},
-    {id:3, name:'ethika', email:'ethika@gmail.com'},
-    {id:2, name:'ethikahin', email:'ethikahin@gmail.com'},
+    {id:2, name:'ethika', email:'ethika@gmail.com'},
+    {id:3, name:'ethikahin', email:'ethikahin@gmail.com'},
 ]
 
 app.get('/',(req, res)=>{
     res.send('users management  server is running ')
 })
+     
 
 app.get('/users', (req, res)=>{
     res.send(users)
+})
+
+app.post('/users', (req, res)=>{
+    console.log('post api hitting');
+    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser)
+    res.send(newUser)
 })
 
 app.listen(port , () =>{
